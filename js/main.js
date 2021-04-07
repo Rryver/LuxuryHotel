69b5407dd7e01b19eb721e12ba3470eca6bbd53f
Alexandr Kolosov <ryver.rainege@gmail.com>
2021-04-07 17:50:31 +0400

    added some adaptive styles

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ function calculatePadding() {
         socialLinks_icon.style.paddingRight = (15 - socialLinks_icon.width) + "px";
     }
 }
+
+
+function menuOpenClose() {
+    let menu = document.getElementsByClassName("nav-menu")[0];
+    if (menu.style.display !== "none") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
